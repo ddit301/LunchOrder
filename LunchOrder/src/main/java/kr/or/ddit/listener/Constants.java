83436f5825dc8247785c87296f07900b30f63982
ShanePark <psh40963@naver.com>
2021-05-06 20:27:20 +0900

    Delete all lunch order projects to make new one

--- LunchOrder/src/main/java/kr/or/ddit/listener/Constants.java
+++
@@ -1,6 +0,0 @@
-package kr.or.ddit.listener;
-
-public class Constants {
-	public static final String SESSIONCOUNTATTRNAME = "sessionCount";
-	public static final String USERLISTATTRNAME = "userList";
-}
