7ff13c63dbd92940bb0a40875276d54003a15445
KrGil <kanjjang89@naver.com>
2021-05-06 19:43:21 +0900

    add lunchOrder project :) ezpz

---
+++ LunchOrder/src/main/java/kr/or/ddit/listener/Constants.java
@@ -0,0 +1,6 @@
+package kr.or.ddit.listener;
+
+public class Constants {
+	public static final String SESSIONCOUNTATTRNAME = "sessionCount";
+	public static final String USERLISTATTRNAME = "userList";
+}
